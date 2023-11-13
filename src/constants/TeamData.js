307da64524656314e1617603334d5abf71f7e68c
binllionaire <bug20474@gmail.com>
2023-11-14 05:31:13 +0900

    add : ✨ 20개 팀 데이터 추가

--- src/constants/TeamData.js
+++ src/constants/TeamData.js
@@ -1,7 +1,102 @@
 export const teamData = [
   {
     id: 1,
+    logoSrc: "/club/club_arsenal.png",
+    teamName: "아스날",
+  },
+  {
+    id: 2,
+    logoSrc: "/club/club_astonvilla.png",
+    teamName: "아스톤 빌라",
+  },
+  {
+    id: 3,
+    logoSrc: "/club/club_bournemouth.png",
+    teamName: "본머스",
+  },
+  {
+    id: 4,
+    logoSrc: "/club/club_brentford.png",
+    teamName: "브렌트포드",
+  },
+  {
+    id: 5,
+    logoSrc: "/club/club_brighton.png",
+    teamName: "브라이튼",
+  },
+  {
+    id: 6,
+    logoSrc: "/club/club_burnley.png",
+    teamName: "번리",
+  },
+  {
+    id: 7,
+    logoSrc: "/club/club_chelsea.png",
+    teamName: "첼시",
+  },
+  {
+    id: 8,
+    logoSrc: "/club/club_crystalpalace.png",
+    teamName: "팰리스",
+  },
+  {
+    id: 9,
+    logoSrc: "/club/club_everton.png",
+    teamName: "에버튼",
+  },
+  {
+    id: 10,
+    logoSrc: "/club/club_fulham.png",
+    teamName: "풀럼",
+  },
+  {
+    id: 11,
+    logoSrc: "/club/club_liverpool.png",
+    teamName: "리버풀",
+  },
+  {
+    id: 12,
+    logoSrc: "/club/club_lutonTown.png",
+    teamName: "루턴",
+  },
+  {
+    id: 13,
+    logoSrc: "/club/club_manchester.png",
+    teamName: "맨유",
+  },
+  {
+    id: 14,
+    logoSrc: "/club/club_mancity.png",
+    teamName: "맨시티",
+  },
+  {
+    id: 15,
+    logoSrc: "/club/club_newcastle.png",
+    teamName: "뉴캐슬",
+  },
+  {
+    id: 16,
+    logoSrc: "/club/club_nottingham.png",
+    teamName: "노팅엄",
+  },
+  {
+    id: 17,
+    logoSrc: "/club/club_sheffield.png",
+    teamName: "셰필드",
+  },
+  {
+    id: 18,
     logoSrc: "/club/club_tottenham.png",
     teamName: "토트넘",
+  },
+  {
+    id: 19,
+    logoSrc: "/club/club_westham.png",
+    teamName: "웨스트햄",
+  },
+  {
+    id: 20,
+    logoSrc: "/club/club_wolverhampton.png",
+    teamName: "울버햄튼",
   },
 ];
