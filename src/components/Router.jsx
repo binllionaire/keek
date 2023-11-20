1d39fa6aa939cf5194689c07c3c2009e5bb9ee0c
binllionaire <bug20474@gmail.com>
2023-11-21 00:37:36 +0900

    feat : ✨ 경기 일정 페이지 라우트 설정

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PlayerSettingPage from "../pages/PlayerSettingPage";
 import RecordPage from "../pages/RecordPage";
+import SchedulePage from "../pages/SchedulePage";
 import StartPage from "../pages/StartPage";
 import TeamSettingPage from "../pages/TeamSettingPage";
 const Router = () => {
@@ -11,6 +12,7 @@ const Router = () => {
         <Route path="/team" element={<TeamSettingPage />}></Route>
         <Route path="/player" element={<PlayerSettingPage />}></Route>
         <Route path="/record" element={<RecordPage />}></Route>
+        <Route path="/schedule" element={<SchedulePage />}></Route>
       </Routes>
     </BrowserRouter>
   );
