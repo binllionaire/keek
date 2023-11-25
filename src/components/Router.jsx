5b0e65e72f7d81467cafb0df7c9944731ee7de7f
binllionaire <bug20474@gmail.com>
2023-11-25 14:42:52 +0900

    release : 🚀 배포 세팅

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PlayerSettingPage from "../pages/PlayerSettingPage";
 import RecordPage from "../pages/RecordPage";
-import SchedulePage from "../pages/SchedulePage";
+// import SchedulePage from "../pages/SchedulePage";
 import StartPage from "../pages/StartPage";
 import TeamSettingPage from "../pages/TeamSettingPage";
 const Router = () => {
@@ -12,7 +12,7 @@ const Router = () => {
         <Route path="/team" element={<TeamSettingPage />}></Route>
         <Route path="/player" element={<PlayerSettingPage />}></Route>
         <Route path="/record" element={<RecordPage />}></Route>
-        <Route path="/schedule" element={<SchedulePage />}></Route>
+        {/* <Route path="/schedule" element={<SchedulePage />}></Route> */}
       </Routes>
     </BrowserRouter>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App.jsx";
-import GlobalStyle from "./styles/GlobalStyle.js";
+import GlobalStyle from "./styles/globalStyle.js";
 import theme from "./styles/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
