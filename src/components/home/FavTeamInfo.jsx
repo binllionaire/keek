5195b78b7a569e819f40a1f2103222ec94f29592
binllionaire <bug20474@gmail.com>
2023-12-04 20:25:17 +0900

    feat : ✨ 최애 팀 정보 UI 구현

---
+++ src/components/home/FavTeamInfo.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+const FavTeamInfo = () => {
+  return (
+    <FavTeamWrapper>
+      <PageIntroWrapper>
+        <Title>토트넘 홋스퍼</Title>
+        <Intro>현재 4위</Intro>
+      </PageIntroWrapper>
+      <TeamLogo src="/club/club_tottenham.png" />
+    </FavTeamWrapper>
+  );
+};
+
+export default FavTeamInfo;
+
+const FavTeamWrapper = styled.div`
+  position: relative;
+  display: flex;
+  height: 200px;
+`;
+const TeamLogo = styled.img`
+  position: absolute;
+  top: 50px;
+  right: 0px;
+
+  width: 100px;
+
+  filter: brightness(0) invert(1);
+  filter: brightness(0) invert(1);
+`;
+
+const PageIntroWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-top: 40px;
+`;
+
+const Title = styled.h2`
+  font-family: "Pretendard-Black";
+  font-size: ${({ theme }) => theme.fontSize.xl};
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+const Intro = styled.p`
+  font-family: "Pretendard-Regular";
+  font-size: ${({ theme }) => theme.fontSize.m};
+  line-height: 24px;
+  color: ${({ theme }) => theme.colors.lightText};
+  text-align: start;
+`;
