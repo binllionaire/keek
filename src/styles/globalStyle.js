3801e1f492466ab11907fab0efbcfd6a3ec26564
binllionaire <bug20474@gmail.com>
2023-12-04 20:27:58 +0900

    fix : 💄 폰트 적용 수정

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -7,41 +7,6 @@ import PPRETENDARD_SEMIBOLD from "../assets/fonts/PretendardStd-SemiBold.woff";
 
 const GlobalStyle = createGlobalStyle`
 * {
-    @font-face {
-        font-family: 'Pretendard-Black';
-        font-weight: 900;
-        font-style: normal; 
-        src: url(${PPRETENDARD_BLACK}) format('woff');
-    }
-
-    @font-face {
-        font-family: 'Pretendard-Bold';
-        font-weight: 700;
-        font-style: normal; 
-        src: url(${PPRETENDARD_BOLD}) format('woff');
-    }
-
-    @font-face {
-        font-family: 'Pretendard-Medium';
-        font-weight: 500;
-        font-style: normal; 
-        src: url(${PPRETENDARD_MEDIUM}) format('woff');
-    }
-
-    @font-face {
-        font-family: 'Pretendard-Regular';
-        font-weight: 400;
-        font-style: normal; 
-        src: url(${PPRETENDARD_REGULAR}) format('woff');
-    }
-
-    @font-face {
-        font-family: 'Pretendard-SemiBold';
-        font-weight: 600;
-        font-style: normal; 
-        src: url(${PPRETENDARD_SEMIBOLD}) format('woff');
-    }
-
     box-sizing : border-box;
 }
 
@@ -112,5 +77,40 @@ table {
 	border-spacing: 0;
 	border-collapse: collapse;
 }
+
+  @font-face {
+      font-family: 'Pretendard-Black';
+      font-weight: 900;
+      font-style: normal; 
+      src: local("PretendardStd-Black"),url(${PPRETENDARD_BLACK}) format('woff');
+  }
+
+  @font-face {
+      font-family: 'Pretendard-Bold';
+      font-weight: 700;
+      font-style: normal; 
+      src: local("PretendardStd-Bold"),url(${PPRETENDARD_BOLD}) format('woff');
+  }
+
+  @font-face {
+      font-family: 'Pretendard-Medium';
+      font-weight: 500;
+      font-style: normal; 
+      src: local("PretendardStd-Medium"),url(${PPRETENDARD_MEDIUM}) format('woff');
+  }
+
+  @font-face {
+      font-family: 'Pretendard-Regular';
+      font-weight: 400;
+      font-style: normal; 
+      src: local("PretendardStd-Regular"),url(${PPRETENDARD_REGULAR}) format('woff');
+  }
+
+  @font-face {
+      font-family: 'Pretendard-SemiBold';
+      font-weight: 600;
+      font-style: normal; 
+      src: local("PretendardStd-SemiBold"),url(${PPRETENDARD_SEMIBOLD}) format('woff');
+  }
 `;
 export default GlobalStyle;
